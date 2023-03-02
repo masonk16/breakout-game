337b786da5717b2df80be9507024f26820528b71
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-02 21:07:48 +0200

    Direction controls for paddle.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import turtle as tr
+from paddle import Paddle
 
 screen = tr.Screen()
 screen.setup(width=1200, height=600)
 screen.bgcolor('black')
 screen.title('Breakout')
+screen.tracer(0)
+
+paddle = Paddle()
+
+screen.listen()
+screen.onkey(key='Left', fun=paddle.move_left)
+screen.onkey(key='Right', fun=paddle.move_right)
+
+screen.update()
 
 tr.mainloop()
